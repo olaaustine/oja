1f6222f3826b31bc09dbeff784c86839b0773761
Ola Austine <olaaustine@Olas-MacBook-Pro.local>
2025-10-04 21:44:33 +0100

    feat: changes to the setting

--- oja/settings.py
+++ oja/settings.py
@@ -96,14 +96,14 @@ WSGI_APPLICATION = 'oja.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
-#DATABASES = {"default": dj_database_url.parse(os.environ["MYSQL_URL"])}
+DATABASES = {"default": dj_database_url.parse(os.environ["MYSQL_URL"])}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -138,9 +138,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
